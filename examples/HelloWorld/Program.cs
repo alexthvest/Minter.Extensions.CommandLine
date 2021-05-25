059751f183cc67561f74a799d75bdc6798622947
alexthvest <alexthvest@gmail.com>
2021-05-25 17:42:29 +0300

    feat: add examples

---
+++ examples/HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Minter.Extensions.CommandLine;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var program = new CommandLineApplication
+            {
+                Name = "minter",
+                Description = "Minter Server CLI tool",
+                Version = new Version(0, 0, 1),
+            };
+            
+            program.OnExecute(() =>
+            {
+                program.ShowHelp();
+            });
+
+            program.Execute(args);
+        }
+    }
+}
